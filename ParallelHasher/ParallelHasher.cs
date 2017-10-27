4d1f03b3f5ff9185ecb185462599fd69c1be9940
VladimirMaltsev <vov.maltsev@gmail.com>
2017-10-27 22:11:26 +0300

    supplements ways for primenums mining by tasks

--- ParallelHasher/ParallelHasher.cs
+++ ParallelHasher/ParallelHasher.cs
@@ -1,74 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Diagnostics;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace ParallelProgramming
+namespace PrimeNumsMining
 {
-    class ParallelHasher
+    class PrimeNumsMiner
     {
         static void Main(string[] args)
         {
-            string path;
-            while ((path = Console.ReadLine()) != "exit") { 
-                if (File.Exists(path) || Directory.Exists(path))
-                {
-                    string hash = GetPathHash(path);
-                    Console.WriteLine(hash);
-                }
-                else Console.WriteLine("directory does not exists");
-            }
-        }       
+            Console.WriteLine("Enter left and right boarders of a search range: ");
+            int l = Convert.ToInt32(Console.ReadLine());
+            int r = Convert.ToInt32(Console.ReadLine());
+            /*
+            Stopwatch sw = Stopwatch.StartNew();
+            ThreadMiner(l, r);
+            sw.Stop();
+            Console.WriteLine("the prime numbers mining is done. Time: {0}", sw.Elapsed);
+            Console.ReadKey();*/
 
-        static string GetPathHash (String path)
-        {
-            StringBuilder sBuilder = new StringBuilder();
-            
-            using (MD5 md5Hash = MD5.Create())
-            {
-                string hash = GetMd5Hash(md5Hash, path);
-                sBuilder.Append(hash);
-            }
+            Stopwatch sw = Stopwatch.StartNew();
+            List<int> list = TaskMiner(l, r);
+            sw.Stop();
+            Console.WriteLine("the prime numbers mining is done. Time: {0}", sw.Elapsed);
 
-            string[] elems;
-            try {
-                elems = Directory.EnumerateDirectories(path).ToArray();
-            }
-            catch (Exception ex) {
-                elems = new string[0];
-            }
 
-            Task<string>[] tasks = new Task<string>[elems.Length]; 
-
-            for (int i = 0; i < elems.Length; ++ i)
-            {
-                string elem = elems[i];
-                tasks[i] = Task.Run(() => GetPathHash(elem));
-            }
-
-            Task.WaitAll(tasks);
-            for (int i = 0; i < tasks.Length; i ++)
-            {
-                string elem_hash = tasks[i].Result;
-                sBuilder.Append(elem_hash);
-            }
-            return sBuilder.ToString();
+            PrintList(list);
+            Console.ReadKey();
         }
 
-        static string GetMd5Hash(MD5 hasher, string path)
+        static bool IsPrime(int num)
         {
-            byte[] data = hasher.ComputeHash(Encoding.UTF8.GetBytes(path));
-            StringBuilder sBuilder = new StringBuilder();
-
-            for (int i = 0; i < data.Length; i ++)
+            if (num % 2 == 0 && num != 2 || num == 1) { return false; }
+            for (int i = 3; i <= Math.Round(Math.Sqrt(num)); i += 2)
             {
-                sBuilder.Append(data[i].ToString("x2"));
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        static List<int> ThreadMiner(int l, int r)
+        {
+            bool[] are_nums_prime = new bool[r + 1];
+            for (int j = 0; j < are_nums_prime.Length; j++)
+                are_nums_prime[j] = false;
+
+            int thread_count = 7;
+            List<Thread> threads = new List<Thread>();
+
+            int step = (r - l) / thread_count;
+
+            int i = l;
+            for (; i < r - (2 * step - 1); i += step)
+            {
+                int left = i;
+                int right = i + step - 1;
+                Thread thread = new Thread(() => CheckPrimeNumsInRange(ref are_nums_prime, left, right));
+                threads.Add(thread);
+                thread.Start();
+            }
+            Thread lastthread = new Thread(() => CheckPrimeNumsInRange(ref are_nums_prime, i, r));
+            threads.Add(lastthread);
+            lastthread.Start();
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
             }
 
-            return sBuilder.ToString();
+            return MinePrimeNums(are_nums_prime);
+        }
+
+        static List<int> TaskMiner(int l, int r)
+        {
+            bool[] are_nums_prime = new bool[r + 1];
+            for (int j = 0; j < are_nums_prime.Length; j++)
+                are_nums_prime[j] = false;
+
+            int task_count = 7;
+            List<Task> tasks = new List<Task>();
+
+            int step = (r - l) / task_count;
+
+            int i = l;
+            for (; i < r - (2 * step - 1); i += step)
+            {
+                int left = i;
+                int right = i + step - 1;
+                Task task = new Task(() => CheckPrimeNumsInRange(ref are_nums_prime, left, right));
+                tasks.Add(task);
+                task.Start();
+            }
+            Task lasttask = new Task(() => CheckPrimeNumsInRange(ref are_nums_prime, i, r));
+            tasks.Add(lasttask);
+            lasttask.Start();
+
+            foreach (Task ftask in tasks)
+            {
+                ftask.Wait();
+            }
+
+            return MinePrimeNums(are_nums_prime);
+        }
+
+        static ThreadPoolMiner(int l, int r)
+        {
+
+        }
+
+        static List<int> MinePrimeNums(bool[] are_nums_prime)
+        {
+            List<int> primeNums = new List<int>();
+            for (int i = 0; i < are_nums_prime.Length; i++)
+                if (are_nums_prime[i])
+                    primeNums.Add(i);
+            return primeNums;
+        }
+
+
+
+        static void CheckPrimeNumsInRange(ref bool[] are_nums_prime, int l, int r)
+        {
+            for (int i = l; i <= r; i++)
+            {
+                are_nums_prime[i] = IsPrime(i);
+            }
+
+        }
+
+        static void PrintList(List<int> l)
+        {
+            foreach (int i in l)
+            {
+                Console.Write("{0} ", i);
+            }
         }
     }
 }
