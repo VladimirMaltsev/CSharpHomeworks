32c72a98e47f9f77b6f1333de7ca2da5e470a45c
VladimirMaltsev <vov.maltsev@gmail.com>
2017-11-29 19:38:06 +0300

    add test code

---
+++ HtmlParser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace HtmlParser
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stUri = Console.ReadLine();
+            var parser = new HtmlParser();
+            
+            var pages = parser.GetHttpPagesDepth(new Page[]{new Page(stUri)}, 1).Result;
+            foreach (var p in pages)
+            {
+                Console.WriteLine("Link: {0}   |>>    byte count = {1}", p.GetUri(), p.GetContent().Result.Length);
+            }
+            
+            Console.ReadKey();
+        }
+        
+        
+    }
+}
