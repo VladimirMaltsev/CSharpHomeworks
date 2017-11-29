2d85ef71288d9ab55e6719b7e2287e622510c08b
VladimirMaltsev <vov.maltsev@gmail.com>
2017-11-29 19:36:38 +0300

    Create class wrapper for links

---
+++ HtmlParser/Page.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace HtmlParser
+{
+    public class Page
+    {
+        private readonly Uri uri;
+
+        public Page(string link)
+        {
+            uri = new Uri(link);
+        }
+        public Uri GetUri()
+        {
+            return uri;
+        }
+
+        public async Task<String> GetContent()
+        {
+            String html = "";
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    html = await client.DownloadStringTaskAsync(uri);
+                }
+                catch (Exception ex)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Write("(!) Exception >> {1} {0}", uri, ex.Message);
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.WriteLine();
+                }
+                
+            }
+            return html;
+        }
+    }
+}
