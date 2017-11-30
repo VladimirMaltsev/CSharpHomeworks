780ff4b15b7b3e592ab1c44644374be1357099a1
VladimirMaltsev <vov.maltsev@gmail.com>
2017-12-01 01:26:02 +0300

    create tree node class

---
+++ ParallelBSTree/ParallelBSTree/Node.cs
@@ -0,0 +1,27 @@
+﻿namespace ParallelBSTree
+{
+    public class Node
+    {
+        public Node left;
+        public Node right;
+        public Node parent;
+
+        public int data;
+        public int key;
+        
+        public Node(int key, int val)
+        {
+            this.key = key;
+            this.data = val;
+            this.left = null;
+            this.right = null;
+            this.parent = null;
+        }
+        
+        //конструктор, в котором можно указать родителя
+        public Node(int key, int data, Node parent) : this(key, data)
+        {
+            this.parent = parent;
+        }
+    }
+}
