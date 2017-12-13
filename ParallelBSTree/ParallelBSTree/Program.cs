dc29914368c63b14afbbec4177bbabdc6c489a3f
VladimirMaltsev <vov.maltsev@gmail.com>
2017-12-13 21:46:17 +0300

    add time-test

--- ParallelBSTree/ParallelBSTree/Program.cs
+++ ParallelBSTree/ParallelBSTree/Program.cs
@@ -8,38 +8,119 @@ namespace ParallelBSTree
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        static void Main()
         {
-            var r = new Random();
-            int[] arr = new int[10];
-            for (var i = 0; i < arr.Length; i ++) 
-                arr[i] = r.Next() % 100;
-           
-           
-            var tree = new BsTree();
-            var dog = Stopwatch.StartNew();
-            var tasks = new List<Task>();
-            for (var i = 0; i < arr.Length; i++)
+            int min = 0;
+            int max = 1000000;
+            int[] insertKeys = new int[max];
+            int[] deleteKeys = new int[max];
+            int[] searchKeys = new int[max];
+            
+            Random randNum = new Random();
+            
+            for (var i = 0; i < insertKeys.Length; i++)
             {
-                var i1 = i;
-                var task = new Task(()  => tree.Insert(arr[i1], arr[i1]));
-                tasks.Add(task);
-                task.Start();
+                insertKeys[i] = randNum.Next(min, max);
             }
-            Task.WaitAll(tasks.ToArray());
-            dog.Stop();
-            Console.WriteLine("Parallel insert of {1} elems done. Time elapsed: {0}", dog.Elapsed, arr.Length);
-            tree.PrintTree(tree._root);
-            tree = new BsTree();
-            dog.Restart();
-            for (var i = 0; i < arr.Length; i++)
+            
+            for (var i = 0; i < deleteKeys.Length; i++)
             {
-                tree.Insert(arr[i], arr[i]);
+                int j = randNum.Next(min, max);
+                deleteKeys[i] = insertKeys[j];
             }
-            dog.Stop();
-            Console.WriteLine("Sequential insert of {1} elems done. Time elapsed: {0}", dog.Elapsed, arr.Length);
-            tree.PrintTree(tree._root);
-            Console.ReadKey();
+            
+            for (var i = 0; i < searchKeys.Length; i++)
+            {
+                int j = randNum.Next(min, max);
+                searchKeys[i] = insertKeys[j];
+            }
+            
+            
+
+            var seqTree = new BsTree();;
+            var parTree = new BsTree();
+
+            //смешанная вставка и удаление
+//            int[] arr = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+//            var tasks = new List<Task>();
+//            for (int i = 0; i < arr.Length; i++)
+//            {
+//                var i1 = i;
+//                tasks.Add(Task.Run(() => parTree.Insert(i1, i1)));
+//                if (i > 2)
+//                {
+//                   tasks.Add(Task.Run(() => parTree.Remove(i1 - 3)));
+//                }
+//                
+//            }
+//            
+//            Task.WaitAll(tasks.ToArray());
+//            parTree.PrintTree(parTree.Root);
+            
+            //производительность
+            Stopwatch time1 = Stopwatch.StartNew();
+           
+            foreach (int key in insertKeys)
+            {
+                seqTree.Insert(key, key);
+            }
+            
+            time1.Stop();
+
+            Console.WriteLine("Sequential insert: {0}", time1.Elapsed);
+            
+            Stopwatch time2 = Stopwatch.StartNew();
+            
+            foreach (int key in deleteKeys)
+            {
+                seqTree.Remove(key);
+            }
+
+            time2.Stop();
+
+            Console.WriteLine("Sequential delete: {0}", time2.Elapsed);
+
+            Stopwatch time4 = Stopwatch.StartNew();
+
+            Parallel.ForEach (insertKeys, key => {    
+                parTree.Insert(key, key);
+            });
+            
+            Stopwatch time3 = Stopwatch.StartNew();
+            
+            foreach (int key in searchKeys)
+            {
+                seqTree.Search(key);
+            }
+
+            time3.Stop();
+
+            Console.WriteLine("Sequential search: {0}", time3.Elapsed);
+            
+            time4.Stop();
+
+            Console.WriteLine("Concurrent insert: {0}", time4.Elapsed);
+
+            Stopwatch time5 = Stopwatch.StartNew();
+            
+            Parallel.ForEach (deleteKeys, key => {
+                parTree.Remove(key);
+            });
+            
+            time5.Stop();
+
+            Console.WriteLine("Concurrent delete: {0}", time5.Elapsed);
+            
+            Stopwatch time6 = Stopwatch.StartNew();
+            
+            Parallel.ForEach (searchKeys, key => {
+                parTree.Search(key);
+            });
+            
+            time6.Stop();
+
+            Console.WriteLine("Concurrent search: {0}", time6.Elapsed);
+            
         }
     }
 }
